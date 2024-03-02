2529d483f8c45f0da039627a8ca53af0f4a8dbc9
DilR7 <erictianto12@gmail.com>
2024-03-02 12:06:25 +0700

    Add Hover on Card

--- scripts/places.js
+++ scripts/places.js
@@ -1,105 +1,117 @@
-places = {}
-resorts = {}
+places = {};
+resorts = {};
 
-const placeObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show-img')
-            placeObserver.unobserve(entry.target)
-        } 
-    })
-})
+const placeObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show-img");
+      placeObserver.unobserve(entry.target);
+    }
+  });
+});
 
-const resortObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show-resort')
-            resortObserver.unobserve(entry.target)
-        } 
-    })
-})
+const resortObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show-resort");
+      resortObserver.unobserve(entry.target);
+    }
+  });
+});
 
-document.querySelectorAll('.accordion-button').forEach(button => {
-    button.addEventListener('click', () => {
-        const accordionContent = button.nextElementSibling;
+document.querySelectorAll(".accordion-button").forEach((button) => {
+  button.addEventListener("click", () => {
+    const accordionContent = button.nextElementSibling;
 
-        button.classList.toggle('active');
+    button.classList.toggle("active");
 
-        if (button.classList.contains('active')) {
-            accordionContent.style.display = 'block';
-            button.querySelector('.icon').textContent = '-';
-        } else {
-            accordionContent.style.display = 'none';
-            button.querySelector('.icon').textContent = '+';
-        }
-    });
+    if (button.classList.contains("active")) {
+      accordionContent.style.display = "block";
+      button.querySelector(".icon").textContent = "-";
+    } else {
+      accordionContent.style.display = "none";
+      button.querySelector(".icon").textContent = "+";
+    }
+  });
 });
 
 async function fetchPlaces() {
-    try {
-        const response = await fetch('scripts/json/places.json')
-        if (!response.ok) {
-            throw new Error(`Failed to fetch JSON. Status: ${response.status}`)
-        }
-        const data = await response.json()
-        places = data.places
-        console.log(places)
-    } 
-    catch (error) {
-        console.error('Error fetching JSON:', error.message)
+  try {
+    const response = await fetch("scripts/json/places.json");
+    if (!response.ok) {
+      throw new Error(`Failed to fetch JSON. Status: ${response.status}`);
     }
+    const data = await response.json();
+    places = data.places;
+    console.log(places);
+  } catch (error) {
+    console.error("Error fetching JSON:", error.message);
+  }
 }
 
 async function fetchResorts() {
-    try {
-        const response = await fetch('scripts/json/hotels.json')
-        if (!response.ok) {
-            throw new Error(`Failed to fetch JSON. Status: ${response.status}`)
-        }
-        const data = await response.json()
-        resorts = data.hotels
-        console.log(resorts)
+  try {
+    const response = await fetch("scripts/json/hotels.json");
+    if (!response.ok) {
+      throw new Error(`Failed to fetch JSON. Status: ${response.status}`);
     }
-    catch (error) {
-        console.error('Error fetching JSON:', error.message)
-    }
+    const data = await response.json();
+    resorts = data.hotels;
+    console.log(resorts);
+  } catch (error) {
+    console.error("Error fetching JSON:", error.message);
+  }
 }
 
 function insertPlaces() {
-    console.log(places)
-    let leftContainer = document.querySelector(".left-container")
-    let rightContainer = document.querySelector(".right-container")
+  console.log(places);
+  let leftContainer = document.querySelector(".left-container");
+  let rightContainer = document.querySelector(".right-container");
 
-    const baseDelay = 100
+  const baseDelay = 100;
 
-    places.forEach((place, index) => {
-        if (index % 2 == 0) {
-            leftContainer.innerHTML += `<div class="card">
+  places.forEach((place, index) => {
+    if (index % 2 == 0) {
+      leftContainer.innerHTML += `<div class="card">
+    <div class="image-container">
+        <img src="${place.image}" class="place-img" id="${
+        place.id
+      }" style="animation-delay: calc(${index / 2 + 1} * ${baseDelay}ms)">
+        <div class="place-name">${place.name}
+        <p>Read More</p></div>
+    </div>
+</div>`;
+    } else {
+      rightContainer.innerHTML += `<div class="card">
+        <div class="image-container">
             <img src="${place.image}" class="place-img" id="${
-                place.id
-              }" style="animation-delay: calc(${index / 2 + 1} * ${baseDelay}ms)">
-          </div>`
-        }
-        else {
-            rightContainer.innerHTML += `<div class="card"> <img src="${place.image}" class="place-img" id="${place.id}" style="animation-delay: calc(${(index / 2) + 1} * ${baseDelay}ms)"</div>>`
-        }
-    })
-    document.querySelectorAll('.place-img').forEach(img => {
-        img.addEventListener('click', () => {
-            localStorage.setItem('Place', JSON.stringify(places.find(place => place.id == img.id)))
-            window.location.href = 'place.html'
-        })
-    })
-    const leftImg = leftContainer.querySelectorAll('img')
-    const rightImg = rightContainer.querySelectorAll('img')
-    leftImg.forEach(img => placeObserver.observe(img))
-    rightImg.forEach(img => placeObserver.observe(img))
+        place.id
+      }" style="animation-delay: calc(${index / 2 + 1} * ${baseDelay}ms)">
+            <div class="place-name">${place.name}
+            <p>Read More</p></div>
+        </div>
+    </div>`;
+    }
+  });
+  document.querySelectorAll(".place-img").forEach((img) => {
+    img.addEventListener("click", () => {
+      localStorage.setItem(
+        "Place",
+        JSON.stringify(places.find((place) => place.id == img.id))
+      );
+      window.location.href = "place.html";
+    });
+  });
+  const leftImg = leftContainer.querySelectorAll("img");
+  const rightImg = rightContainer.querySelectorAll("img");
+  leftImg.forEach((img) => placeObserver.observe(img));
+  rightImg.forEach((img) => placeObserver.observe(img));
 }
 
 function insertResorts() {
-    const resortContainer = document.querySelector(".resort-container")
-    resorts.forEach((resort, index) => {
-        resortContainer.innerHTML += `
+  const resortContainer = document.querySelector(".resort-container");
+  resorts.forEach((resort, index) => {
+    resortContainer.innerHTML += `
             <div class="resort" style="animation-delay: ${index * 200}ms">
                 <div class="resort-img-container">
                 <img src="${resort.image}" class="resort-img">
@@ -112,16 +124,16 @@ function insertResorts() {
                 <p class="resort-name">${resort.name}</p>
                 <p class="resort-location">📍 ${resort.location}</p>
             </div>
-        `
-    })
-    const resortContainers = resortContainer.querySelectorAll('.resort')
-    resortContainers.forEach(resort => resortObserver.observe(resort))
+        `;
+  });
+  const resortContainers = resortContainer.querySelectorAll(".resort");
+  resortContainers.forEach((resort) => resortObserver.observe(resort));
 }
 
 fetchPlaces().then(() => {
-    insertPlaces()
-})
+  insertPlaces();
+});
 
 fetchResorts().then(() => {
-    insertResorts()
-})
+  insertResorts();
+});
